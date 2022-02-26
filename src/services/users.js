eeb49aa689a29910fd16e7e64c34ed9f6ccd6b5c
Lucas <lucas.taferdevs@gmail.com>
2022-02-26 01:31:10 -0300

    bug-fixes

--- src/services/users.js
+++ src/services/users.js
@@ -1,7 +1,7 @@
 import Api from "./api";
 
 const UsersService = {
-  register: (params) => Api.post("/users/register", params),
+  register: (params) => Api.post("/register", params),
   login: async (params) => {
     const response = await Api.post("/users/login", params);
     localStorage.setItem("user", JSON.stringify(response.data.user));
